e870b31c8c8360d0a31d49ff1dbf4c5082ccd20a
Martin Woodward <martinwoodward@github.com>
2021-05-02 16:19:54 +0100

    Initial fork from alexellis code

---
+++ app/app.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import json
+import os, sys
+from sensors import sensors
+from camera import camera
+from specimen import specimen
+
+if __name__ == "__main__":
+    print("Starting growlab-bfaulty")
+
+    config = {}
+    try:
+        with open("./config.json") as f:
+            config = json.loads(f.read())
+    except Exception as e:
+        sys.stderr.write("Error: {}".format(e))
+        sys.exit(1)
+
+    print("Loaded config, saving images every {} seconds to {}".format( config["images"]["interval_seconds"], config["images"]["output_directory"]))
+
+    sensors = sensors()
+    readings = sensors.get_readings()
+
+    print(readings)
+
+    cam = camera(config["images"])
+    frame = cam.get_frame()
+
+    pwd = os.getcwd()
+    output_path = pwd + "/html"
+
+    try:
+       os.mkdir(output_path)
+    except:
+       pass
+
+    spec = specimen(config["text"], config["images"])
+    spec.save_image("{}/image.jpg".format(pwd), frame, readings)
+    spec.save_html("{}/image.jpg".format(pwd), output_path, readings)
---
+++ app/sensors.py
@@ -0,0 +1,46 @@
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+from bme280 import BME280
+from TCS34725 import TCS34725
+from ADS1015 import ADS1015
+
+import time
+
+class sensors:
+    def __init__(self):
+        self.bus = SMBus(1)
+        self.bme280 = BME280(i2c_dev=self.bus)
+        self.ads = ADS1015()
+        self.light = TCS34725(0X29, debug=False)
+
+
+    def get_readings(self):
+        # Ignore first results & initialize sensors
+        temperature = self.bme280.get_temperature()
+        pressure = self.bme280.get_pressure()
+        humidity = self.bme280.get_humidity()
+        soil0 = self.ads.read(0)
+        light.TCS34725_init()
+
+        time.sleep(0.1)
+
+        temperature = self.bme280.get_temperature()
+        pressure = self.bme280.get_pressure()
+        humidity = self.bme280.get_humidity()
+        soil0 = self.ads.read(0)
+        lux = self.light.Get_Lux()
+        colorTemp = self.light.Get_ColorTemp()
+
+        time_str = time.strftime("%H:%M:%S")
+
+        return {
+            "time": time_str,
+            "temperature": temperature,
+            "pressure": pressure,
+            "humidity": humidity,
+            "soil0": soil0,
+            "lux": lux,
+            "colourTemp": colorTemp
+        }
